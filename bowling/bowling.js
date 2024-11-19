bb18a3a5e6557f4f260bdb4503d0fb7b4f4caa5e
Mike880002 <miky882002@gmail.com>
2024-11-19 19:47:36 +0100

    ho aggiunto qualcosa

--- bowling/bowling.js
+++ bowling/bowling.js
@@ -1,34 +1,36 @@
-let bowling = { 'players': 
-    [ 
-        {'name': 'Nicola', scores: []},
-        {'name': 'Luca', scores: []},
-        {'name': 'Matteo', scores: []},
+let bowling = {
+    players: [
+        { name: 'Nicola', scores: [] },
+        { name: 'Luca', scores: [] },
+        { name: 'Matteo', scores: [] },
     ],
-        'ottieniPunteggio' : function(){
-            for (let tiro = 0; tiro <= 10; tiro++) {
-                this.players.forEach(player => {
-                    let punteggio= Math.floor(Math.random() * 10) + 1;
-                    if(punteggio == 10){
-                        alert('HAI FATTO IL STRIKE');
-                        punteggio += 5;
-                        player.scores.push(punteggio);
-                        tiro++;
-                    } else {
-                        alert(`Hai totalizzato: ${punteggio}`)
-                        player.scores.push(punteggio);
-                        tiro++;
-                    }
-                });
+    ottieniPunteggio: () => {
+        bowling.players.forEach(player => { 
+            for (let tiro = 1; tiro <= 10; tiro++) {
+                let punteggio = Math.floor(Math.random() * 10) + 1;
+                if (punteggio === 10) {
+                    alert(`${player.name} ha fatto uno STRIKE!`);
+                    punteggio += 5;
+                } else {
+                    alert(`${player.name} ha totalizzato: ${punteggio}`);
+                }
+                player.scores.push(punteggio);
             }
-        }}
-
-        let btnGame = document.querySelector('#btnGame');
-
-        btnGame.addEventListener('click', ()=> {
-            bowling.ottieniPunteggio(player);
-            alert(scores);
         });
 
+        // Mostra i punteggi finali
+        bowling.players.forEach(player => {
+            console.log(`${player.name} - Punteggi: ${player.scores.join(', ')}`);
+        });
+    }
+};
+
+// Associazione del bottone
+let btnGame = document.querySelector('#btnGame');
+btnGame.addEventListener('click', () => {
+    bowling.ottieniPunteggio();
+});
+   
 
 
         
